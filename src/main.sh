7fd50d558f3e4e9a290fd3d2e74b8e4d61645b59
shota-h <syo06ta18@gmail.com>
2020-08-19 15:37:05 +0900

    bug fix

--- src/main.sh
+++ src/main.sh
@@ -1,7 +1,7 @@
-python3 src/temporal_disentanglement.py --ex all_loss --d2ae --batch 32 --triplet
+python3 src/temporal_disentanglement.py --ex wo_triplet --d2ae --batch 64 --epoch 500
 wait
-python3 src/temporal_disentanglement.py --ex all_loss_rev --d2ae --batch 32 --triplet
+python3 src/temporal_disentanglement.py --ex wo_triplet_rev --batch 64 --d2ae --rev --epoch 500
 wait
-python3 src/temporal_disentanglement.py --ex wo_triplet --d2ae --batch 64
+python3 src/temporal_disentanglement.py --ex all_loss --d2ae --batch 32 --triplet --epoch 500
 wait
-python3 src/temporal_disentanglement.py --ex wo_triplet_rev --batch 64 --d2ae --rev
+python3 src/temporal_disentanglement.py --ex all_loss_rev --d2ae --batch 32 --triplet --rev --epoch 500
